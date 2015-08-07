0dc474fcf64d022e219428067b3425b97324df85
Vitor Capela <dodecaphonic@gmail.com>
2015-08-07 11:03:14 -0300

    Fixes referencing tiles in the north and east hemispheres.

--- lib/such_great_heights/srtm_conversions.rb
+++ lib/such_great_heights/srtm_conversions.rb
@@ -1,8 +1,8 @@
 module SuchGreatHeights
   module SrtmConversions
     def lon_lat_to_tile(lon, lat)
-      tlat = (lat.to_i - 1).abs.to_s.rjust(2, "0")
-      tlon = (lon.to_i - 1).abs.to_s.rjust(3, "0")
+      tlat = coord_to_tile_ref(lat).rjust(2, "0")
+      tlon = coord_to_tile_ref(lon).rjust(3, "0")
 
       format("%s%s%s%s.hgt.zip", north_south(lat), tlat, east_west(lon), tlon)
     end
@@ -15,6 +15,10 @@ module SuchGreatHeights
     end
 
     private
+
+    def coord_to_tile_ref(coord)
+      (coord < 0 ? coord - 1 : coord).to_i.abs.to_s
+    end
 
     def north_south(lat)
       lat <= 0 ? "S" : "N"
--- spec/unit/srtm_conversions_spec.rb
+++ spec/unit/srtm_conversions_spec.rb
@@ -12,8 +12,8 @@ describe SuchGreatHeights::SrtmConversions do
         tile_name = subject.lon_lat_to_tile(longitude, latitude)
         lon, lat  = subject.tile_to_lon_lat(tile_name)
 
-        expect(lat).to eq(latitude.floor.to_i - 1)
-        expect(lon).to eq(longitude.floor.to_i - 1)
+        expect(lat).to eq(latitude < 0 ? latitude.floor.to_i - 1 : latitude.floor.to_i)
+        expect(lon).to eq(longitude < 0 ? longitude.floor.to_i - 1 : longitude.floor.to_i)
       end
     end
   end
