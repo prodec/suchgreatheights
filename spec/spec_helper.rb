c6c33e658fbb005df7a1e4da8f0da37f93b34535
Vitor Capela <dodecaphonic@gmail.com>
2015-08-28 22:34:48 -0300

    Adds coverage report using CodeClimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require "rspec"
 require "generative"
 require "degenerate"
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.start
 
 require_relative "../lib/such_great_heights"
 require_relative "generators"
